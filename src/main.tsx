5519a7d229ca7dd5c226e8a59324d70df8cda65c
Victor Oliva <olivarra1@gmail.com>
2025-03-30 15:21:23 +0200

    using subtrate client

--- src/main.tsx
+++ src/main.tsx
@@ -23,101 +23,27 @@ const chain = await smoldot.addChain({
 
 const provider = getSmProvider(chain);
 
-// let id = 0;
-// const connection = provider((msg) => {
-//   const parsed = JSON.parse(msg);
-//   console.log(parsed);
+import { createClient } from "@polkadot-api/substrate-client";
 
-//   if (parsed.params?.result?.event === "stop") {
-//     sendFollow();
-//   }
-// });
+const client = createClient(provider);
 
-function correlate(provider: JsonRpcProvider) {
-  let id = 0;
+const chainHead = client.chainHead(
+  true,
+  async (evt) => {
+    console.log("chainHead evt", evt);
 
-  let followId: string = "";
-  const bodyRequests = new Map<number, (value: string[]) => void>();
-  const ongoingRequests = new Map<string, (value: string[]) => void>();
+    if (evt.type === "initialized") {
+      const [block] = evt.finalizedBlockHashes;
 
-  const connection = provider((msgStr) => {
-    const msg = JSON.parse(msgStr);
+      const metadata = await chainHead.call(block, "Metadata_metadata", "");
+      console.log(metadata);
 
-    if (
-      msg.params?.result?.event === "initialized" ||
-      msg.params?.result?.event === "newBlock"
-    ) {
-      console.log(msg.params.result);
+      chainHead.unpin(evt.finalizedBlockHashes);
+    } else if (evt.type === "newBlock") {
+      chainHead.unpin([evt.blockHash]);
     }
-
-    if (msg.id === followRequestId) {
-      console.log(msg);
-      followId = msg.result;
-    } else if (bodyRequests.has(msg.id)) {
-      if (msg.result.result != "started") {
-        console.log(msg);
-        throw new Error("Something went wrong");
-      }
-
-      const resolve = bodyRequests.get(msg.id)!;
-      bodyRequests.delete(msg.id);
-      ongoingRequests.set(msg.result.operationId, resolve);
-    } else {
-      const operationId = msg.params?.result?.operationId;
-
-      if (operationId && ongoingRequests.has(operationId)) {
-        const resolve = ongoingRequests.get(operationId)!;
-        ongoingRequests.delete(operationId);
-        resolve(msg.params.result.value);
-      }
-    }
-  });
-
-  const followRequestId = id++;
-  connection.send(
-    JSON.stringify({
-      id: followRequestId,
-      jsonrpc: "2.0",
-      method: "chainHead_v1_follow",
-      params: [true],
-    })
-  );
-
-  return {
-    getBody(hash: string): Promise<string[]> {
-      const bodyReqId = id++;
-
-      connection.send(
-        JSON.stringify({
-          jsonrpc: "2.0",
-          id: bodyReqId,
-          method: "chainHead_v1_body",
-          params: [followId, hash],
-        })
-      );
-
-      return new Promise<string[]>((resolve) => {
-        bodyRequests.set(bodyReqId, resolve);
-      });
-    },
-  };
-}
-
-const correlationExercise = correlate(provider);
-
-window.correlationExercise = correlationExercise;
-
-// const result = await correlationExercise.getBody("0x0123485874");
-
-// const sendFollow = () => {
-//   console.log("send follow");
-//   connection.send(
-//     JSON.stringify({
-//       id: ++id,
-//       jsonrpc: "2.0",
-//       method: "chainHead_v1_follow",
-//       params: [false],
-//     })
-//   );
-// };
-// sendFollow();
+  },
+  (err) => {
+    console.log(err);
+  }
+);
