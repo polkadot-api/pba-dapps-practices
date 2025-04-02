3bf9c8bb0820e7ea84d3f957c664df286d7dad42
Victor Oliva <olivarra1@gmail.com>
2025-04-02 12:22:02 +0200

    switchMap

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Observable, share } from "rxjs";
+import { Observable, share, Subscription } from "rxjs";
 
 const observable$ = new Observable<number>((subscriber) => {
   let value = 0;
@@ -31,51 +31,53 @@ const map =
       return () => subscription.unsubscribe();
     });
 
-const take =
-  <T,>(amount: number) =>
-  (source$: Observable<T>) =>
-    new Observable<T>((subscriber) => {
-      let sent = 0;
-      const subscription = source$.subscribe({
-        next(value) {
-          subscriber.next(value);
-          sent++;
+const switchMap =
+  <T, R>(mapFn: (value: T) => Observable<R>) =>
+  (source: Observable<T>) =>
+    new Observable<R>((subscriber) => {
+      let innerSubscription: Subscription | null = null;
+      let isOuterComplete = false;
+      let isInnerComplete = false;
 
-          if (sent === amount) {
+      const subscription = source.subscribe({
+        next: (v) => {
+          const innerObservable = mapFn(v);
+
+          innerSubscription?.unsubscribe();
+          isInnerComplete = false;
+          innerSubscription = innerObservable.subscribe({
+            next(value) {
+              subscriber.next(value);
+            },
+            error(err) {
+              subscriber.error(err);
+            },
+            complete() {
+              isInnerComplete = true;
+              if (isOuterComplete) {
+                subscriber.complete();
+              }
+            },
+          });
+        },
+        error: (e) => subscriber.error(e),
+        complete: () => {
+          isOuterComplete = true;
+          if (isInnerComplete) {
             subscriber.complete();
           }
-        },
-        error(err) {
-          subscriber.error(err);
-        },
-        complete() {
-          subscriber.complete();
         },
       });
 
       return () => {
+        innerSubscription?.unsubscribe();
         subscription.unsubscribe();
       };
     });
 
-class Potato {
-  constructor(private value: number) {}
-
-  foo() {
-    console.log(this.value);
-  }
-}
-
-const potato = new Potato(10);
-const fn = potato.foo;
-fn.call(potato);
-
-// const sharedObservable$ = observable$.pipe(
-//   share(),
-//   map((x) => x * 2),
-//   take(5)
-// );
-
-// const subscription = sharedObservable$.subscribe((res) =>
-//   console.log("A next", res)
+// const bounty$ = interval(1000).pipe(
+//   take(100),
+//   switchMap((id) => {
+//     return typedApi.query.Bounties.Bounty.watchValue(id);
+//   })
 // );
