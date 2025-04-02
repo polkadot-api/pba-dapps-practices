17eea38a2bae09283ce0f20f5361b521d1c6dc89
Victor Oliva <olivarra1@gmail.com>
2025-04-02 11:33:07 +0200

    operators

--- src/main.tsx
+++ src/main.tsx
@@ -1,73 +1,49 @@
-import { chainSpec } from "polkadot-api/chains/westend2";
-import { getSmProvider } from "polkadot-api/sm-provider";
-import { start } from "polkadot-api/smoldot";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-// import { JsonRpcProvider } from "@polkadot-api/json-rpc-provider";
-import { JsonRpcProvider } from "polkadot-api/ws-provider/web";
+import { Observable, share } from "rxjs";
 
-export function withLogsRecorder(
-  persistLog: (msg: string) => void,
-  // Provider wrapped
-  provider: JsonRpcProvider
-): JsonRpcProvider {
-  return (onMessage) => {
-    const connection = provider((msg) => {
-      persistLog("<< " + msg);
-      onMessage(msg);
+const observable$ = new Observable<number>((subscriber) => {
+  let value = 0;
+  const token = setInterval(() => {
+    subscriber.next(value);
+    value++;
+  }, 1000);
+
+  return () => {
+    clearInterval(token);
+  };
+});
+
+const map =
+  <T, R>(mapValue: (value: T) => R) =>
+  (source$: Observable<T>) =>
+    new Observable<R>((subscriber) => {
+      const subscription = source$.subscribe({
+        next(value) {
+          subscriber.next(mapValue(value));
+        },
+        error(err) {
+          subscriber.error(err);
+        },
+        complete() {
+          subscriber.complete();
+        },
+      });
+
+      return () => subscription.unsubscribe();
     });
 
-    return {
-      disconnect() {
-        connection.disconnect();
-      },
-      send(message) {
-        persistLog(">> " + message);
-        connection.send(message);
-      },
-    };
-  };
-}
+const take =
+  <T,>(amount: number) =>
+  (source$: Observable<T>) =>
+    new Observable<T>((subscriber) => {
+      // TODO
+    });
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const sharedObservable$ = observable$.pipe(
+  share(),
+  map((x) => x * 2),
+  take(5)
 );
 
-const smoldot = start({
-  forbidWs: true,
-});
-
-const chain = await smoldot.addChain({
-  chainSpec,
-});
-
-const provider = getSmProvider(chain);
-
-import { createClient } from "@polkadot-api/substrate-client";
-
-const client = createClient(provider);
-
-const chainHead = client.chainHead(
-  true,
-  async (evt) => {
-    console.log("chainHead evt", evt);
-
-    if (evt.type === "initialized") {
-      const [block] = evt.finalizedBlockHashes;
-
-      const metadata = await chainHead.call(block, "Metadata_metadata", "");
-      console.log(metadata);
-
-      chainHead.unpin(evt.finalizedBlockHashes);
-    } else if (evt.type === "newBlock") {
-      chainHead.unpin([evt.blockHash]);
-    }
-  },
-  (err) => {
-    console.log(err);
-  }
+const subscription = sharedObservable$.subscribe((res) =>
+  console.log("A next", res)
 );
